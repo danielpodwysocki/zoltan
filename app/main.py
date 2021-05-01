c47d40c8413bc7c133a28db7d5da9ee7b8968483
Daniel Podwysocki <daniel@contact24.pl>
2021-05-01 17:27:43 +0200

    make the indentation PEP8 compliant

--- app/main.py
+++ app/main.py
@@ -10,16 +10,16 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def on_event():
 
-  """Handles an event from Google Chat."""
-  event = request.get_json()
-  if event['type'] == 'ADDED_TO_SPACE' and not event['space']['singleUserBotDm']:
-    text = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
-  elif event['type'] == 'MESSAGE':
-    text = 'You said: `%s`' % event['message']['text']
-  else:
-    return
-  return json.jsonify({'text': text})
+    """Handles an event from Google Chat."""
+    event = request.get_json()
+    if event['type'] == 'ADDED_TO_SPACE' and not event['space']['singleUserBotDm']:
+        text = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
+    elif event['type'] == 'MESSAGE':
+        text = 'You said: `%s`' % event['message']['text']
+    else:
+        return
+    return json.jsonify({'text': text})
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
