9e43c97e2c353e200dea2ca7cb63a179e64e6485
Daniel Podwysocki <daniel@contact24.pl>
2021-05-01 22:47:29 +0200

    add help responses for different modules

--- app/modules/help.py
+++ app/modules/help.py
@@ -5,6 +5,13 @@ class Handler:
     id = 1
     def command(message):
         response = "Here's a list of available commands:\n\
-                    /help - displays this help page \n\
-                    /ssh [resource] [action] - a utility to check the availibility of hosts, run /help ssh to learn more"
+            /help - displays this help page \n\
+            /ssh [resource] [action] - a utility to check the availibility of hosts, run `/help ssh` to learn more"
+
+        help_responses = {
+            "ssh": "Run `/ssh [machine's name] status` to see if the machine is reachable"
+        }
+        #if we have a help response for the passed arg, fill the response with it
+        if message and message in help_responses:
+            response = help_responses[message]
         return response
