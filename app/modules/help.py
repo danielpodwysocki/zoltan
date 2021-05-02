dbad5f009526a4875dcf7240efa445ad1d559b33
Daniel Podwysocki <daniel@contact24.pl>
2021-05-02 19:20:11 +0200

    add help info for the reboot command

--- app/modules/help.py
+++ app/modules/help.py
@@ -6,10 +6,12 @@ class Handler:
     def command(self, message):
         response = "Here's a list of available commands:\n\
             /help - displays this help page \n\
-            /ssh [resource] [action] - a utility to check the availibility of hosts, run `/help ssh` to learn more"
+            /ssh [machine name] - a utility to check the availibility of hosts, run `/help ssh` to learn more\n\
+            /reboot [machine name] - a utility that reboots a machine, run `/help reboot` to learn more" 
 
         help_responses = {
-            "ssh": "Run `/ssh [machine's name]` to see if the machine is reachable"
+            "ssh": "Run `/ssh [machine's name]` to see if the machine is reachable",
+            "reboot": "Run `/reboot ['machine's name]` to reboot the machine"
         }
         #if we have a help response for the passed arg, fill the response with it
         
